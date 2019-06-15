2e031d131d906f5386f3a3981f6523525aaed1c0
izaird <iza_alex3@hotmail.com>
2019-06-15 10:01:33 -0500

    reboot?

--- README.md
+++
@@ -1,2 +0,0 @@
-# WebCamp
-Proyecto web para el entendimiento de HTML5, CSS3, JS, JQuery, Ajax
--- includes/funciones/bd_conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root','m0th3l3td3lg4','gdlwebcamp');
-
-  if($conn->connect_error){
-    echo $error ->$conn->connect_error;
-  }
- ?>
